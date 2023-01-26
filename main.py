8e9fc853939b4dac7ab6fd2b28f9beb634074d85
Dashavod <dvodolazska@morebis.net>
2023-01-26 15:50:48 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-
-
-
-def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json(force=True, silent=True, cache=True)  # see https://flask.palletsprojects.com/en/2.0.x/api/ get_json Parse data as JSON
-    #in v.2.1 of the Flask framework they changed the impl of the get_json and if the content type if not provided it returns http 400 - BAD Requests, therefore we are usong now force=true and silent=true
-
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
-
-
-import json
 from kernel import kernel
-import aiohttp
 import requests
 import os
 from dialogflow import detect_intent_texts
@@ -47,11 +22,8 @@ def root(request):
             print(res.json())
             return res.text
     if engine == "dialogflow":
-        print("1")
         response = detect_intent_texts("devtorium-bot-e9vy", "123456", {text}, "en")
         return {"message": response}
-
-
 
 
     return {"message": "Incorrect type value, please change and try again"}
