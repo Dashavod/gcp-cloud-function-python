26b14ed2bcca2d15a376cfc2b0ddc1823469ba57
Dashavod <dvodolazska@morebis.net>
2023-02-15 18:40:19 +0200

    storage images

--- stableDiffusion.py
+++ stableDiffusion.py
@@ -1,16 +1,25 @@
+import datetime
+from datetime import timedelta
+
 from flask import send_file
 from huggingface_hub.inference_api import InferenceApi
 import os
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from io import StringIO, BytesIO
 
 import json
 import requests
 
+import firebase_admin
+from firebase_admin import storage
+from requests import ReadTimeout
+
+cred_obj = firebase_admin.credentials.Certificate('cred.json')
+default_app = firebase_admin.initialize_app(cred_obj, {"storageBucket": "gcf-sources-334279855271-europe-west3"})
+
 def stableDiffusion(prompt: str):
 
     api_token = "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"
-    print(api_token)
     API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
     headers = {"Authorization": f"Bearer {api_token}"}
 
@@ -20,11 +29,29 @@ def stableDiffusion(prompt: str):
     # img = inference(inputs=prompt)
     # print(img)
     # # img.save("images/img2.png", format='JPEG', quality=75)
-    img = Image.open(BytesIO(response.content))
-    return serve_pil_image(img)
+    # imageToStore(response.content)
+    try:
+        return {"image_url": imageToStore(response.content)}
+    except ReadTimeout:
+        return "stable diffusion doesn't return image"
+
 
 def serve_pil_image(pil_img):
+
     img_io = BytesIO()
     pil_img.save(img_io, 'JPEG', quality=75)
     img_io.seek(0)
+    #imageToStore(pil_img)
     return send_file(img_io, mimetype='image/jpeg')
+
+def imageToStore(res):
+    exp = timedelta(hours=3)
+    bucket = storage.bucket()
+    blob_new = bucket.blob(str(datetime.datetime.now()))
+    blob_new.upload_from_string(res, content_type="image/jpeg")
+
+    blob_url = blob_new.generate_signed_url(expiration=exp)
+    return blob_url
+
+
+    #blob.download_to_filename("/images/test.png")
--- test.py
+++ test.py
@@ -5,9 +5,9 @@ with open('train.txt') as f:
     lines = f.readlines()
 print(lines)
 translator = Translator()
-translated = [translator.translate(phrase, dest="uk") for phrase in lines]
+translated = {translator.translate(phrase, dest="uk") for phrase in lines}
 print([item.text for item in translated])
-[detect_intent_texts("uk-devtorium-q-a-alek","scr3", {item.text}, "uk") for item in translated]
+[detect_intent_texts("uk-devtorium-q-a-alek","scr5", {item.text}, "uk") for item in translated]
 # for str in translated:
 #     detect_intent_texts("uk-devtorium-q-a-alek", "scr", {str}, "ua")
 #detect_intent_texts("devtorium-bot-e9vy", "scr", translated, "en")
