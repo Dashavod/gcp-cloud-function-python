5c1393527b3855faf790358464325e23876dbfbd
Dashavod <dvodolazska@morebis.net>
2023-03-01 16:47:31 +0200

    add options to POST

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def root(request):
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             'Access-Control-Max-Age': '3600'
         }
 
