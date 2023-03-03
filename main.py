92cdfa639414cf7313878659ba6e45712cf8ea35
Dashavod <dvodolazska@morebis.net>
2023-03-03 13:53:03 +0200

    exeptions

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from jsonToStorage import jsonToStore
 from kernel import kernel
 import requests
@@ -9,47 +7,41 @@ from stableDiffusion import stableDiffusion
 from templates import template_basic
 import functions_framework
 from firestore import *
+
+
 @functions_framework.http
 def root(request):
-
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST',
-            'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-            'Access-Control-Allow-Credentials': 'true'
-        }
-
-        return ('', 204, headers)
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true'
-    }
-
     data = request.get_json()
     print(data)
     text = data["message"]
     user = data["sender"]
     engine = data["type"]
+
     if engine == "gpt":
-        response = kernel(f"{template_basic} \n provide information about {text} in the same format as above", "123456", 0.27)
-        res =eval(response)
+        response = kernel(f"{template_basic} \n provide information about {text} in the same format as above", user,
+                          0.27)
+        try:
+            res = eval(response)
+        except Exception as e:
+            print(e)
+            return f"OpenAI provide information with wrong stucture, please retry\n {e}"
+
         id = add_document_to_firestore(res)
         document = find_document_in_firestore_by_id(id)
         return document
+
     if engine == "rasa":
-            payload = {'message': text, 'sender': user}  # res = await task(payload)
-            res = requests.post(os.getenv('RASA_URL'), json=payload)
-            print(res.json())
-            return res.text
+        payload = {'message': text, 'sender': user}  # res = await task(payload)
+        res = requests.post(os.getenv('RASA_URL'), json=payload)
+        print(res.json())
+        return res.text
+
     if engine == "dialogflow":
         response = detect_intent_texts("devtorium-bot-e9vy", user, {text}, "en")
         return {"message": response}
+
     if engine == "stable_diffusion":
         response = stableDiffusion(text)
-        return  response
+        return response
 
     return {"message": "Incorrect type value, please change and try again"}
