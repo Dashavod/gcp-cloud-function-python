87d4bd93ae490b7ed4949f5ba20e480331903f9b
Dashavod <dvodolazska@morebis.net>
2023-03-30 11:41:51 +0300

    fix email gen

--- services/openAI/emailTextGenerator.py
+++ services/openAI/emailTextGenerator.py
@@ -1,3 +1,4 @@
+import html2text
 from aihub.kernel import kernel
 from db.firestore import add_document_to_firestore
 from templates.email import Email
@@ -5,7 +6,8 @@ from templates.templates import get_template
 
 
 def emailTextGenerator(data,user):
-    template = get_template(data["message"],type="emailsTemplate")
+    template = get_template(html2text.html2text(data["message"]),type="emailsTemplate")
+    print("template",template)
     response = kernel(template, user, 0.8,max_tokens=3634)
     print(response)
     # try:
@@ -14,9 +16,10 @@ def emailTextGenerator(data,user):
     #     print(f"OpenAI provide information with wrong stucture, please retry\n {e}")
     #     return f"OpenAI provide information with wrong stucture, please retry\n {e}"
     email = Email({
-        "company_name":data["message"],
+        "company_name":"dev",
         "content": response,
-        "email_type": "first welcome email"
+        "email_type": "rephrase"
     })
     add_document_to_firestore(email, "Emails")
     return email.__dict__
+
--- templates/templates.py
+++ templates/templates.py
@@ -2,12 +2,12 @@
 def get_template(company, type:str, industries=None):
     with open(f"templates/prompts/{type}.txt", "r") as f:
         template = f.read()
-        new = template.replace('**your_company**', company)
-        print("new", new)
     match(type):
         case "companyInfo":
             print("temp comp")
+            new = template.replace('**your_company**', company)
             return new.replace('**your_industries**', industries)
-        case "emailTemplate":
-            return new
+        case "emailsTemplate":
+            print("temp comp")
+            return template.replace('**your_email**', company)
     return template
