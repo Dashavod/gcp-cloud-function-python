fda61c90172ecd257c13ad5c843ca3c9eba92964
Dashavod <dvodolazska@morebis.net>
2023-04-04 12:27:15 +0300

    too long message

--- services/openAI/emailTextGenerator.py
+++ services/openAI/emailTextGenerator.py
@@ -8,7 +8,10 @@ from templates.templates import get_template
 def emailTextGenerator(data,user):
     template = get_template(html2text.html2text(data["message"]),type="emailsTemplate")
     print("template",template)
-    response = kernel(template, user, 0.8,max_tokens=3634)
+    if len(template) > 3500:
+        print("template too long")
+        return "Too long message, please shorten the text"
+    response = kernel(template, user, 0.8,max_tokens=4000)
     print(response)
     # try:
     #     res = eval(response)
